4c210881f834f3080092edabaf253dade7f1479b
ahamedzoha <ahamed.zoha@gmail.com>
2024-06-22 17:41:47 +0600

    updated nginx and docker compose

--- be/src/main.ts
+++ be/src/main.ts
@@ -5,6 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.setGlobalPrefix('api');
   await app.listen(3001);
 }
 bootstrap();
