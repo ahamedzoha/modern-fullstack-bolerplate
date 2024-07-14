e56720eec7b2bf91366ebad674e8f087968d9e4e
ahamedzoha <ahamed.zoha@gmail.com>
2024-07-14 15:08:51 +0600

    updated be port

--- be/src/main.ts
+++ be/src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3001);
+  await app.listen(3003);
 }
 bootstrap();
