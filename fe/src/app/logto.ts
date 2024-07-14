508ad49cbe0bcf055a0582c4eb11f341cb3bac7f
ahamedzoha <ahamed.zoha@gmail.com>
2024-07-14 16:07:16 +0600

    added volume mount for fe hot reloading

---
+++ fe/src/app/logto.ts
@@ -0,0 +1,8 @@
+export const logtoConfig = {
+  endpoint: "http://localhost:3003/",
+  appId: "mga760he09d1zvd9ux9af",
+  appSecret: "6ZOvG7HVS4rB61cwUf907zHM4SKwyLZ5",
+  baseUrl: "http://localhost:3000", // Change to your own base URL
+  cookieSecret: "YCyw3Gnt0bFcm1VChyowrR9vt5zLaiwe", // Auto-generated 32 digit secret
+  cookieSecure: process.env.NODE_ENV === "production",
+}
--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -1,9 +1,6 @@
 import { LandingNav } from "@/components/modules/landing-nav"
-import { getUsers } from "@/server/get-users"
 
 export default async function Home() {
-  const users = await getUsers()
-  console.log(users?.data?.map((user) => user.firstName))
   return (
     <main className="w-full">
       <LandingNav />
@@ -17,18 +14,9 @@ export default async function Home() {
         h-dvh
       "
       >
-        <h1 className="text-9xl font-bold">Welcome to the home page</h1>
+        <h1 className="text-9xl font-bold">Welcome to the home</h1>
         <div className="mt-4">
           <h2 className="text-4xl font-bold">Users</h2>
-          <ul>
-            {users?.data?.map((user) => (
-              <li key={user.id}>
-                {user.firstName} {user.lastName}
-              </li>
-            ))}
-          </ul>
-
-          <p>Total users: {users?.totalCount}</p>
         </div>
       </div>
     </main>
